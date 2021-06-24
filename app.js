04e874e528936be1978a923e32b2e7a7bb427b33
Redavela <kodadreda67@gmail.com>
2021-06-25 01:11:37 +0200

    récuperation des données pour les cartes

--- app.js
+++ app.js
@@ -1,46 +1,27 @@
-function displayList(){
-    let container = document.querySelector('.row')
-    container.innerHTML=''
-    for(i=0 ; i<recipes.length;i++){
-        container.appendChild(createCard(recipes[i].name,recipes[i].time, recipes[i].description))
-        for(let j=0 ;j<recipes[i].ingredients.length ;j++){
-            container.appendChild(addIngredients(recipes[i].ingredients[j].ingredient, recipes[i].ingredients[j].quantity, recipes[i].ingredients[j].quantite, recipes[i].ingredients[j].unit  ))
-            }
-            
-    }
-}
-displayList()
-function addIngredients(ingredient, quantity, quantite, unit) {
-    const div = document.createElement('div')
-    const pIngredient = document.createElement('p')
-    div.className = 'card'
-    pIngredient.className = 'card-text'
-    pIngredient.innerText = ingredient + ' : ' 
-    div.appendChild(pIngredient)
-    return div
 
+const container = document.querySelector('.row')
+
+function createCard(){
+    container.innerHTML = recipes.map(recette =>
+        `
+        <div class="card col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-0 col-lg-4" style="width: 18rem;">
+            <div class="card-title">
+                <h1> ${recette.name} </h1>
+                <span><i class="fas fa-clock" ></i> ${recette.time} min</span>
+            </div>
+            <div class="card-body">
+                <div class="ingredient">
+                    ${recette.ingredients.map(element =>
+                         `<div><span><b>${element.ingredient}: </b></span>
+                         <span>${ "quantity" in element ? element.quantity : ""} </span>
+                         <span>${ "unit" in element? element.unit : ""}</span>
+                         </div>`).join('')}
+                </div>
+                <div class="card-text">
+                    <span>${recette.description}</span>
+                </div>        
+            </div>
+        </div>
+        `).join('')
 }
-function createCard(name,time, description){
-    const div = document.createElement('div')
-    const div2 = document.createElement('div')
-    const titre = document.createElement('h5')
-    const para = document.createElement('p')
-    const icon = document.createElement('i')
-    const divTime = document.createElement('div')
-    div.className = 'card'
-    div.style.width = '18rem'
-    div2.className = 'card-body'
-    titre.className = 'card-title'
-    titre.innerText = name
-    para.className ='card-text'
-    para.innerText = description
-    para.className = 'card-time'
-    icon.className = 'fa-solid fa-clock'
-    divTime.innerText = time
-    icon.appendChild(divTime)
-    div2.appendChild(titre)
-    div2.appendChild(icon)
-    div2.appendChild(para)
-    div.appendChild(div2)
-    return div
-}
+createCard()
