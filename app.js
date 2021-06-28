dd26a68f92919908a2c99b8d6e93d1365e1970e5
Redavela <kodadreda67@gmail.com>
2021-06-28 18:05:55 +0200

    Ajustement css  des cartes

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const container = document.querySelector('.row')
 function createCard(){
     container.innerHTML = recipes.map(recette =>
         `
-        <div class="card col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-0 col-lg-4" style="width: 18rem;">
-            <div class="card-title">
-                <h1> ${recette.name} </h1>
-                <span><i class="fas fa-clock" ></i> ${recette.time} min</span>
-            </div>
+        <div class="card col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-0 col-lg-4 " style="width: 18rem;">
+            <div class="card-img"></div>
             <div class="card-body">
-                <div class="ingredient">
+            <header class="card-title">
+                <h2> ${recette.name} </h2>
+                <span><i class="fas fa-clock" ></i> ${recette.time} min</span>
+            </header>
+                <div class="card-ingredient">
                     ${recette.ingredients.map(element =>
                          `<div><span><b>${element.ingredient}: </b></span>
                          <span>${ "quantity" in element ? element.quantity : ""} </span>
