1a5e335f4b03cb0e2f4da6756d42882cd385cc82
Redavela <kodadreda67@gmail.com>
2021-07-13 19:24:51 +0200

    fin du deuxieme algo

--- app.js
+++ app.js
@@ -30,35 +30,82 @@ createCard()
 class Tree{
     constructor(elements){
         this.tree = {}
-        this.objectExplorer(elements)
-        console.log(this.tree);
+        this.objectExplorer(elements, null)
     }
-    addToTree(node){
+    addToTree(node, parent){
         let currentTree = this.tree
         for(const letter of node){
             currentTree[letter] = currentTree[letter] || {}
             currentTree = currentTree[letter]
          }
+        currentTree['_elements'] = currentTree['_elements'] || []
+        currentTree['_elements'].push(parent)
     }
-    objectExplorer(node){
+    objectExplorer(node, parent){
         if(node){
             if(typeof node === 'object' || Array.isArray(node) ){
+                node['_parent'] = parent
                 for(const key in node){
-                    this.objectExplorer(node[key])
+                    if(key !== '_parent'){
+                        this.objectExplorer(node[key], node)
+                    }    
                 }
             }
             else if(typeof node === 'string' && isNaN(parseFloat(node))){
                 const words = node.split(" ")
+                
                 for(const word of words){
-                    this.addToTree(word.toLowerCase())
+                    this.addToTree(word.toLowerCase(), parent)
                 }
             }
         }
    }
+   browseTree(combinaisons, text) {
+    const result = {};
+    var currentTree = this.tree;
+    for (const letter of text) {
+      if (letter in currentTree) {
+        currentTree = currentTree[letter];
+      }
+    }
+    const elements = this.gatherElements(currentTree);
+    for (const element of elements) {
+      for (const combinaison of combinaisons) {
+        if (Object.keys(element).includes(combinaison)) {
+          result[combinaison] = result[combinaison] || [];
+          result[combinaison].push({
+            element: element[combinaison],
+            parent: element,
+          });
+        }
+      }
+    }
+    return result;
+  }
+   gatherElements(tree){
+    let elements = []
+    let toVisit = [tree]
+
+       while(toVisit.length !== 0){
+           const currentTree = toVisit.pop()
+         
+           if("_elements" in currentTree){
+               elements = elements.concat(currentTree._elements);
+           }
+           for( const key in currentTree)
+           {
+               if(key !== '_elements'){
+                toVisit.push(currentTree[key])
+               }
+           }
+       }
+       return elements
+   }
 } 
 
+algoRechercheArbre(['ingredient','name','description'],'tom')
 
-
-function algoRechercheArbre(){
+function algoRechercheArbre(combinaisons,text){
     const tree = new Tree(recipes)
+    return tree.browseTree(combinaisons,text)
 }
